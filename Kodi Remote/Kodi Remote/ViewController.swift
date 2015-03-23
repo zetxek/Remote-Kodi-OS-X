f76310e6452def9332f377779a8a98623ffd3ebe
Adrián Moreno Peña <zetxek@gmail.com>
2015-03-24 00:23:48 +0100

    Project creation. Gitignore setup. Pods added.

---
+++ Kodi Remote/Kodi Remote/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Kodi Remote
+//
+//  Created by Adrián Moreno Peña on 23/03/15.
+//  Copyright (c) 2015 Nyan Software. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
