30dac531aa37eeb6601ec70d0596d6042a3d8408
Adrián Moreno Peña <zetxek@gmail.com>
2015-03-25 00:31:32 +0100

    JSON-RPC invovation and sending to server.

--- Kodi Remote/Kodi Remote/ViewController.swift
+++ Kodi Remote/Kodi Remote/ViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import Cocoa
+import Alamofire
+import SwiftyJSON
 
 class ViewController: NSViewController {
 
@@ -22,6 +24,53 @@ class ViewController: NSViewController {
         }
     }
 
+    @IBAction func btnClick(sender: AnyObject) {
+
+
+        // {"jsonrpc":"2.0","method":"Input.Left","id":1}
+        let parameters = [
+                "jsonrpc": "2.0",
+                "method": "Input.Down",
+                "id": 1
+        ]
+
+        let url : String = "http://192.168.192.8:8080/jsonrpc"
+        let URL = NSURL(string: url)!
+        let mutableURLRequest = NSMutableURLRequest(URL: URL)
+        mutableURLRequest.HTTPMethod = "POST"
+
+        var JSONSerializationError: NSError? = nil
+        mutableURLRequest.HTTPBody = NSJSONSerialization.dataWithJSONObject(parameters, options: nil, error: &JSONSerializationError)
+        mutableURLRequest.setValue("application/json", forHTTPHeaderField: "Content-Type")
+
+        request(mutableURLRequest)
+        .responseJSON
+        { (request, response, data, error) in
+
+            // Request error
+            if(error != nil) {
+
+                println(error)
+
+            }else{
+
+                // No error
+                var json = JSON(data!)
+                println(json)
+
+
+            }
+
+
+            /*if let delegate = self.delegate {
+                var json = JSON(data!)
+                //delegate.didReceiveResult(json)
+                println(json)
+            }*/
+        }
+        
+        
+    }
 
 }
 
